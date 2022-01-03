d8ddcb4a43299ddcc57a896d95e636d525600253
Udit Rai <rai.udit.88@gmail.com>
2022-01-03 09:53:26 +0530

    Added the chuckNorris random Joke generator.

--- src/main/java/com/example/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/com/example/spring5jokesappv2/services/JokeServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.spring5jokesappv2.services;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService{
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl()
+    {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
     @Override
     public String getJokes() {
-        return "Knock Knock! \n Who's there ? \n Your worst nightmare.";
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
